56f17ed16a69b2e7d855b945eec8462267ad9dfe
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-11-21 21:56:15 +0100

    s

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 const app = require("./app");
+
 const DB_PASSWORD = encodeURIComponent("Z@TNhTsS99tR6!Q");
 
 const DB_HOST = `mongodb+srv://agsulimko:${DB_PASSWORD}@clusteragsulimko1.6tm1onp.mongodb.net/hw03-mongodb?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", true);
 
-mongoose.connect(DB_HOST).then(() => {
-  app
-    .listen(3000, () => {
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
       console.log("Database connection successful");
-    })
-    .catch((error) => {
-      console.log(error.message);
-      process.exit(1);
     });
-});
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
