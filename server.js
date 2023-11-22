dac94b6430b154e1555859772d748cdb666ed523
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-11-22 18:16:27 +0100

    s

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const { DB_HOST } = require("./config");
+// const { DB_HOST } = require("./config");
+const { DB_HOST } = process.env;
 // const dotenv = require("dotenv");
 // dotenv.config();
-
+// console.log(process.env);
 // const DB_HOST =
 //   "mongodb+srv://agsulimko:qw54JJJHJG787FFgfh@clusteragsulimko1.6tm1onp.mongodb.net/hw03-mongodb?retryWrites=true&w=majority";
 
