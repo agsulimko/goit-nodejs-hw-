890cef98f3246f117c22d59b5edd980eb2ea4dce
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-11-22 18:03:44 +0100

    s

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 const { DB_HOST } = require("./config");
-// const DB_PASSWORD = encodeURIComponent("Z@TNhTsS99tR6!Q");
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-// const DB_HOST = `mongodb+srv://agsulimko:${DB_PASSWORD}@clusteragsulimko1.6tm1onp.mongodb.net/hw03-mongodb?retryWrites=true&w=majority`;
+// const DB_HOST =
+//   "mongodb+srv://agsulimko:qw54JJJHJG787FFgfh@clusteragsulimko1.6tm1onp.mongodb.net/hw03-mongodb?retryWrites=true&w=majority";
 
-mongoose.set("strictQuery", true);
+// mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
@@ -18,6 +20,3 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
-//  "engines": {
-//     "node": ">=14.20.1"
-//   },
