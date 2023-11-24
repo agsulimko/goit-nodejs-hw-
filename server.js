aa72c6f927e64e55d5d93e0e84450ec9acb7c29c
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-11-24 11:36:02 +0100

    s

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { DB_HOST } = require("./config");
 
 // const { DB_HOST, PORT = 3000 } = process.env;
 
-// mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
