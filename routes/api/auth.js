68b3520fe116f35397d4454bc9de321dc705c065
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-12-04 16:43:33 +0100

    s

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,13 +5,17 @@ const { schemas } = require("../../models/user");
 
 const router = express.Router();
 
-router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post(
+  "/users/register",
+  validateBody(schemas.registerSchema),
+  ctrl.register
+);
 
-router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+router.post("/users/login", validateBody(schemas.loginSchema), ctrl.login);
 
-router.get("/current", authenticate, ctrl.getCurrent);
+router.get("/users/current", authenticate, ctrl.getCurrent);
 
-router.post("/logout", authenticate, ctrl.logout);
+router.post("/users/logout", authenticate, ctrl.logout);
 
 router.patch("/users", authenticate, ctrl.updateStatusSubscription);
 
