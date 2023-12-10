7b9eec9564f2b19e205cd5ae0cbc5305f9bf42d4
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-12-10 13:15:52 +0100

    s

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -16,7 +16,8 @@ const authenticate = async (req, res, next) => {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
 
-    if (!user || !user.token || user.token !== token) {
+    if (!id || !user || !user.token) {
+      // (!id || !user || !user.token || user.token !== token)
       next(HttpError(401, "Not authorized"));
     }
     req.user = user;
